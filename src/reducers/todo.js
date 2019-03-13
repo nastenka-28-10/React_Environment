f76748f01333daadb581ad66de61c5e49aa1156d
Anastasiia Grigorenko <Anastasiia_Grigorenko@epam.com>
2019-03-13 16:14:44 +0300

    delete some unnecessary code

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,16 +1,7 @@
-import { TOGGLE_TODO, ADD_TODO_SUCCESS } from '../constants/todoActionTypes';
+import { ADD_TODO_SUCCESS } from '../constants/todoActionTypes';
 
 const todo = ( state, action ) => {
     switch (action.type) {
-        case TOGGLE_TODO:
-            if(state.id !== action.id)
-                return state;
-            else {
-                return {
-                    ...state,
-                    completed: !state.completed
-                };
-            }
         case ADD_TODO_SUCCESS:
             return {
                 id: action.id,
