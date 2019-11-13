c6fac4cd6fddef8065700eea03339438a7b505f6
Anastasiia Grigorenko <Anastasiia_Grigorenko@epam.com>
2019-11-14 02:51:37 +0300

    comparison functional components and classes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 //import { BrowserRouter } from 'react-router-dom';
@@ -7,14 +7,44 @@ import store from './store/store';
 
 import App from './components/main/App';
 
+class Example1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 0};
+    }
 
-//<Provider store={store}>
-// <App />
-//</Provider>,
+    componentDidMount() {
+        document.title = `Вы нажали ${this.state.count} раз`;
+    }
+
+    componentDidUpdate() {
+        document.title = `Вы нажали ${this.state.count} раз`;
+    }
+
+    render(){
+        return(
+          <div style={{border: "1px solid red"}} onClick={() => {this.setState(({count})=>({count: ++count}))}}>
+              {this.state.count}
+          </div>
+        );
+    }
+}
+
+function Example2() {
+    let [count, setCount] = useState(0);
+    useEffect(()=>{
+        document.title = count;
+    });
+    return (
+        <div style={{border: "1px solid red"}} onClick={() => setCount(++count)}>
+            {count}
+        </div>
+    )
+}
 
 ReactDOM.render(
     <Provider store={store}>
-        <h1>Hello all</h1>
+        <Example2/>
     </Provider>,
     document.getElementById('root')
 );
